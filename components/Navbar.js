4890f1962b506fbde0df9870e67ed3da5a131048
Will <will.worth@gmail.com>
2021-06-21 09:59:31 +0200

    end of monday class

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,26 +1,69 @@
-import Link from "next/link"
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-export function Navbar(){
-    return <>
-   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link className="navbar-brand" href="/" passHref >Navbar</Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+export function Navbar() {
+  let router = useRouter();
+  let searchQuery;
+  let search = function (event) {
+    event.preventDefault();
+    if (searchQuery) {
+      router.push("/search?q=" + searchQuery);
+    }
+  };
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" href="/" passHref>
+          Navbar
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item">
-        <Link href="/login" passHref >Login</Link>
-        <Link href="/signup" passHref >Signup</Link>
-        <Link href="/cart" passHref >Cart</Link>
-      </li>
-    </ul>
-    <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-     <Link href="/search" passHref ><button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button></Link>
-    </form>
-  </div>
-</nav>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link href="/login" passHref>
+                Login
+              </Link>
+              <Link href="/signup" passHref>
+                Signup
+              </Link>
+              <Link href="/cart" passHref>
+                Cart
+              </Link>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              onChange={(e) => {
+                searchQuery = e.target.value;
+              }}
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <Link href="/search" passHref>
+              <button
+                onClick={search}
+                className="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </Link>
+          </form>
+        </div>
+      </nav>
     </>
+  );
 }
--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,105 @@
-export default function Search(){
-    return (
-        <>
-        Search Page 
-        </>
-    )
+import axios from "axios";
+import { useRouter } from "next/router";
+let apiurl = "https://apifromashu.herokuapp.com/api/searchcakes?q=";
+
+export default function Search(props) {
+  //   console.log("props of search cakes", props);
+  var router = useRouter();
+  //   console.log("router ", router);
+  //   console.log("searchresults ", props.searchresults);
+  return (
+    <>
+      Search Page <h1>Showing Results for {router.query.q}</h1>
+    </>
+  );
 }
+
+export async function getServerSideProps(context) {
+  console.log("query from context", context.query);
+  var response = await axios({
+    method: "get",
+    url: apiurl + context.query.q,
+  });
+  return {
+    props: {
+      searchresults: response.data.data,
+    },
+  };
+}
+
+//In next js it is a limitation that we cant pre fetch the data in case of queries for that we may use srver side generation of pages
+
+// receive the query
+// hit the api with query
+// display to user
+
+// As per next js we have learnt data fetching in pre-rendering
+// what types of pre rendering are there
+
+// static generation of pages
+// server side generation of pages
+
+// data fetching is not possible in static generation pf pages in case of query
+// static generation takes place at build time and at build time we can't figure out what query user may ask
+
+// so ,we are going to use server side gneration of pages if want to otherwise normal react concepts
+
+// import axios from "axios";
+// import { useRouter } from "next/router";
+// let apiurl = "https://apifromashu.herokuapp.com/api/searchcakes?q=";
+// export default function Search() {
+//   var router = useRouter();
+//   return (
+//     <>
+//       Search Page
+//       <h1>Showing results for {router.query.q}</h1>
+//     </>
+//   );
+// }
+
+// export function getServerSideProps(context) {
+//   console.log("context: ", context);
+//   var response = await axios({
+//     method: "get",
+//     url: apiurl + `${context.router.query.q}`,
+//   });
+//   return {
+//     props: {
+//       searchresults: response.data.data,
+//     },
+//   };
+// }
+
+// import axios from "axios";
+// import { useRouter } from "next/router";
+// import { useState, useEffect } from "react";
+
+// export default function Search() {
+//   const router = useRouter();
+//   const [result, setResult] = useState([]);
+
+//   useEffect(async () => {
+//     let response = await axios({
+//       method: "get",
+//       url:
+//         process.env.NEXT_PUBLIC_BASE_URL +
+//         `/searchcakes?q=${router.query.cake}`,
+//     });
+//     setResult(response.data.data);
+//   });
+
+//   return (
+//     <>
+//       {result.length &&
+//         result.map((cake) => {
+//           return (
+//             <div key={cake._id}>
+//               <p>Name: {cake.name}</p>
+//               <p>Description: {cake.description}</p>
+//               <p>Price: {cake.price}</p>
+//             </div>
+//           );
+//         })}
+//     </>
+//   );
+// }
